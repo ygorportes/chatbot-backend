379c1305e54b4661d2ca5b7b75cf46f90c891bdf
Ygor Portes <ygorportes3@gmail.com>
2025-04-03 18:41:16 -0300

    first commit

---
+++ src/main/java/com/portestech/chatbotbackend/ChatbotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.portestech.chatbotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatbotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portestech/chatbotbackend/ChatbotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portestech.chatbotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
